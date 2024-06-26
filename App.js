cdd1c73a0e1cb6b6db690eddc98082a16acaa442
Peter Elorm Seth Selormey <pesselormey@st.ug.edu.gh>
2024-06-26 00:24:42 +0000

    bold added

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>My name is Peter Elorm Seth Selormey</Text>
+      <Text style={styles.text}>My name is <Text style={styles.boldText}>Peter Elorm Seth Selormey</Text></Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ccf381',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text:{
+    fontSize: 24,
+    textAlign: 'center',
+  },
+  boldText:{
+    fontWeight: 'bold',
+  }
 });
